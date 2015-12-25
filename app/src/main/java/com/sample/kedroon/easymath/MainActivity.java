a9d16d3f2cbc689cdcb172062ee821de398e7b68
Miguel Figueiredo Marques <miguelkedroon@gmail.com>
2015-12-25 12:42:14 -0400

    First
    Lista de Operacoes

--- app/src/main/java/com/sample/kedroon/easymath/MainActivity.java
+++ app/src/main/java/com/sample/kedroon/easymath/MainActivity.java
@@ -3,13 +3,47 @@ package com.sample.kedroon.easymath;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView list;
+
+    String[] web = {
+            "Google Plus",
+            "Twitter",
+            "Windows",
+            "Bing",
+            "Itunes",
+            "Wordpress",
+            "Drupal"
+    };
+    Integer[] imageId = {
+            R.drawable.skull,
+            R.drawable.skull,
+            R.drawable.skull,
+            R.drawable.skull,
+            R.drawable.skull,
+            R.drawable.skull,
+            R.drawable.skull,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListOfOperations adapter = new ListOfOperations(this,web,imageId);
+        list =(ListView)findViewById(R.id.operations_list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "You Clicked at " + web[+position], Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
