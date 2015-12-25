43eb863cf9c33104248a6ffee9bcad6b8c4ca812
Miguel Figueiredo Marques <miguelkedroon@gmail.com>
2015-12-25 11:45:06 -0400

    Master 1

---
+++ app/src/main/java/com/sample/kedroon/easymath/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sample.kedroon.easymath;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
